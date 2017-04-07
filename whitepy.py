61a6c2da407f6ffbe83cf1f2bf39deb00a747e60
Yasser Nabi <yassersaleemi@gmail.com>
2017-04-07 23:59:21 +0400

    Update whitepy.py to pretty print
    Make it easier to read and debug

--- whitepy.py
+++ whitepy.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
 import whitepy.lexer as lexer
+from whitepy.lexerconstants import *
 
-line = "   \t\n"
-item = lexer.Lexer(line=line)
+import pprint
+pp = pprint.PrettyPrinter(width=41, depth=2, compact=True)
+
+
+with open('./helloworld.ws', 'r') as f:
+    lines = f.read()
+
+item = lexer.Lexer(line=lines)
 item.get_all_tokens()
-print("{}".format(item.tokens))
+index = 0
+for i in item.tokens:
+    index += 1
+    print(index)
+    pp.pprint([x.type if type(x) == "Token" else x for x in i])
