167e51da694b0492e11808cc6db43f24dccc7968
Yasser Nabi <yassersaleemi@gmail.com>
2017-04-08 17:01:15 +0400

    Add debug object
    debug object can be used to represent ws characters as something a bit more
    readable

---
+++ tests/debug_test.py
@@ -0,0 +1,22 @@
+from nose.tools import *
+from whitepy.lexerconstants import *
+import unittest
+from whitepy.debug import Debug as debug
+
+
+class TestLexer(unittest.TestCase):
+
+    def _get_debug(self, line):
+        return debug(line)
+
+    def _sample_ws(self, ws_type):
+        ws_samples = {
+            'valid': "   \t\n",
+            'invalid_int': "   \t",
+            'non_ws': "A   \tB\nC"
+        }
+        return self._get_debug(ws_samples[ws_type])
+
+    def test_get_int(self):
+        dbg = self._sample_ws('valid')
+        assert dbg.__str__() == '..._|'
---
+++ whitepy/debug.py
@@ -0,0 +1,20 @@
+from .lexerconstants import *
+
+
+class Debug(object):
+
+    def __init__(self, string):
+        self.translate_tuple = (
+            (CHAR_MAP['space'], '.'),
+            (CHAR_MAP['tab'], '_'),
+            (CHAR_MAP['lf'], '|'),
+        )
+        self.string = self._translate(string)
+
+    def __str__(self):
+        return self.string
+
+    def _translate(self, string):
+        for i in self.translate_tuple:
+            string = string.replace(i[0], i[1])
+        return string
