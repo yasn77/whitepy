99ab22cf5fcba719dd7d9d87c18c8d93de5591a4
Yasser Nabi <yassersaleemi@gmail.com>
2017-04-23 01:23:05 +0400

    Add IO Class
    Manage WS IO. This class has no tests yet.
    
    TODO: Add IO tests

---
+++ whitepy/ws_io.py
@@ -0,0 +1,27 @@
+import readchar
+import sys
+
+
+class IO(object):
+    def __init__(self, stack):
+        self.stack = stack
+
+    def i_chr(self, heap):
+        self.stack.push(readchar.readchar())
+        heap.set()
+
+    def i_int(self, heap):
+        num = None
+        while type(num) is not int:
+            try:
+                num = int(readchar.readchar())
+            except ValueError:
+                pass
+        self.stack.push(num)
+        heap.set()
+
+    def o_chr(self):
+        sys.stdout.buffer.write(self.stack.pop().encode('utf-8'))
+
+    def o_int(self):
+        sys.stdout.buffer.write(str(self.stack.pop()).encode('utf-8'))
