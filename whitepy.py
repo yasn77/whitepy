aa8e3f3333c8d29d3af38bc73d18518891900ef7
Yasser Nabi <yassersaleemi@gmail.com>
2017-04-24 00:25:01 +0400

    Updated test script to use parser

--- whitepy.py
+++ whitepy.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 import whitepy.lexer as lexer
+import whitepy.parser as ws_parser
 from whitepy.lexerconstants import *
 
 import pprint
 pp = pprint.PrettyPrinter(width=41, depth=2, compact=True)
 
 
+#with open('./count.ws', 'r') as f:
 with open('./helloworld.ws', 'r') as f:
     lines = f.read()
 
 item = lexer.Lexer(line=lines, debug=True)
 item.get_all_tokens()
-index = 0
-for i in item.tokens:
-    index += 1
-    print(index)
-    pp.pprint([x.type if type(x) == "Token" else x for x in i])
+p = ws_parser.Parser(item.tokens)
+p.parse()
+#index = 0
+#for i in item.tokens:
+#    index += 1
+#    pp.pprint([x.type if type(x) == "Token" else x for x in i])
