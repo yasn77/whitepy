c865a9da2fe7795d7fa1d57930c12d5765853ad4
Yasser Nabi <yassersaleemi@gmail.com>
2017-04-29 21:48:39 +0400

    Add input tests

---
+++ tests/test_ws_io_test.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+import unittest
+from whitepy.ws_io import IO as ws_io
+from whitepy.heap import Heap
+from whitepy.stack import Stack
+
+
+class TestIo(unittest.TestCase):
+
+    def setUp(self):
+        self.stack = Stack()
+        self.heap = Heap(self.stack)
+        self.io = ws_io(self.stack)
+
+    @patch('builtins.input', side_effect='TEST'.split())
+    def test_i_chr(self, input):
+        self.stack.push('1')  # Heap address used for stack.pop()
+        self.stack.push('1')  # Heap address for storing input
+        self.io.i_chr(self.heap)
+        self.heap.get()
+        assert self.stack.pop() == 'TEST'
+
+    @patch('builtins.input', side_effect='1')
+    def test_i_int(self, input):
+        self.stack.push('1')  # Heap address used for stack.pop()
+        self.stack.push('1')  # Heap address for storing input
+        self.io.i_int(self.heap)
+        self.heap.get()
+        assert self.stack.pop() == 1
