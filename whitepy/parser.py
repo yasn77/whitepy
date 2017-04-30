9a179a003bf221feb8d0771ff9cc0546097fc781
Yasser Nabi <yassersaleemi@gmail.com>
2017-05-01 00:35:04 +0400

    Correctly create a signed number
    Test for signed value was incorrect and so was removing the sign character from
    the raw whitespace. Since it is a string, can't use pop().

--- whitepy/parser.py
+++ whitepy/parser.py
@@ -53,7 +53,8 @@ class Parser(object):
 
     def _get_value(self, ws_int, signed=False):
         if signed:
-            sign = '-' if NUM_SIGN_CONST[ws_int.pop(0)] == 'NEGATIVE' else ''
+            sign = '-' if NUM_SIGN_CONST[ws_int[0]] == 'NEGATIVE' else ''
+            ws_int = ws_int[1:]
         number = int(''.join([NUM_CONST[i] for i in ws_int]), 2)
         return int('{}{}'.format(sign, number)) if signed else number
 
@@ -99,11 +100,9 @@ class Parser(object):
         while self.instruction_ptr < self.num_of_tokens:
             token = self.token_list[self.instruction_ptr]
             if token[1].type in HAS_ARGS:
-                signed = True if token[1] is 'PUSH' else False
+                signed = True if token[1].type is 'PUSH' else False
                 int_value = self._get_value(token[2].value, signed=signed)
-                # print("Token: {} - Val: {}".format(token[1].type, int_value))
                 self.method_map[token[0].type][token[1].type](int_value)
             else:
-                # print("Token: {}".format(token[1].type))
                 self.method_map[token[0].type][token[1].type]()
             self.instruction_ptr += 1
