6eb0f92bce56550de930611559bdd7c55c906ce3
Yasser Nabi <yassersaleemi@gmail.com>
2017-05-26 23:23:35 +0400

    Increase Test coverage

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -8,11 +8,18 @@ class TestLexer(unittest.TestCase):
     def _get_lexer(self, line):
         return lexer.Lexer(line=line)
 
+    def _get_file_ws(self, filename):
+        with open(filename, 'r') as f:
+            lines = f.read()
+        return lines
+
     def _sample_ws(self, ws_type):
         ws_samples = {
             'valid': "   \t\n",
             'invalid_int': "   \t",
-            'non_ws': "A   \tB\nC"
+            'non_ws': "A   \tB\nC",
+            'calc': self._get_file_ws('./sample_ws/calc.ws'),
+            'fibonacci': self._get_file_ws('./sample_ws/fibonacci.ws')
         }
         return self._get_lexer(ws_samples[ws_type])
 
@@ -40,3 +47,11 @@ class TestLexer(unittest.TestCase):
         t = lexer.tokens
         assert t[0][0].get_type() is 'STACK_MANIPULATION' and \
             t[0][1].get_type() is 'PUSH' and t[0][2].get_type() is 'INT'
+
+    def test_get_const(self):
+        lexer = self._sample_ws('fibonacci')
+        lexer.get_all_tokens()
+        t = lexer.tokens
+        self.assertTrue(False not in [True for i in t if
+                                      type(lexer._get_const(i[1].get_type()) is
+                                           dict)])
